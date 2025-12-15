09dbb64a6641103adef1e23bbae5aada1700bad1
sevketbuyukdemir <sevket.buyukdemir@gmail.com>
2025-12-15 19:53:35 +0300

    BASIC-3 - Security config & User Details Service & Authentication Provider.

---
+++ src/main/java/com/sevketbuyukdemir/basic_authentication/security/config/DevSecurityConfig.java
@@ -0,0 +1,138 @@
+package com.sevketbuyukdemir.basic_authentication.security.config;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.ProviderManager;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.logout.LogoutSuccessHandler;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.io.IOException;
+import java.util.Collections;
+import java.util.List;
+
+@Profile("!prod")
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class DevSecurityConfig {
+    private static final Logger logger = LogManager.getLogger(DevSecurityConfig.class);
+
+    @Bean
+    static GrantedAuthorityDefaults grantedAuthorityDefaults() {
+        return new GrantedAuthorityDefaults("SVKTBYKDMR_");
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(AbstractHttpConfigurer::disable)
+                .anonymous(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(getPublicResources()).permitAll()
+                        .requestMatchers(getAdminResources()).hasRole("ADMIN")
+                        .requestMatchers(getUserResources()).hasRole("USER")
+                        .anyRequest().authenticated())
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration config = new CorsConfiguration();
+                        config.setAllowedOrigins(Collections.singletonList("http://localhost:3000"));
+                        config.setAllowedMethods(Collections.singletonList("*"));
+                        config.setAllowCredentials(true);
+                        config.setAllowedHeaders(Collections.singletonList("*"));
+                        config.setExposedHeaders(List.of("Authorization"));
+                        config.setMaxAge(3600L);
+                        return config;
+                    }
+                }))
+                .sessionManagement(smc -> smc
+                        .invalidSessionUrl("/invalid-session")
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED)
+                        .sessionFixation()
+                        .migrateSession()
+                        .maximumSessions(1)
+                        .maxSessionsPreventsLogin(true)
+                        .expiredUrl("/expired-session")
+                )
+                .logout(lOut -> {
+                    lOut.logoutUrl("/logout")
+                            .logoutSuccessHandler(new CustomLogoutSuccessHandler())
+                            .invalidateHttpSession(true)
+                            .deleteCookies("Authorization");
+                })
+                .securityContext((securityContext) -> securityContext
+                        .requireExplicitSave(true)
+                )
+                .httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder(12);
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        CustomUsernamePwdAuthenticationProvider authenticationProvider = new CustomUsernamePwdAuthenticationProvider(userDetailsService, passwordEncoder);
+        ProviderManager providerManager = new ProviderManager(authenticationProvider);
+        providerManager.setEraseCredentialsAfterAuthentication(false);
+        return providerManager;
+    }
+
+    private String[] getAdminResources() {
+        return new String[]{
+                "/swagger-ui/**",
+                "/user-management/**"
+        };
+    }
+
+    private String[] getUserResources() {
+        return new String[]{
+                "/profile",
+                "/change-my-password"
+        };
+    }
+
+    private String[] getPublicResources() {
+        return new String[]{
+                "/login",
+                "/logout",
+                "/invalid-session",
+                "/expired-session"
+        };
+    }
+
+    private static final class CustomLogoutSuccessHandler implements LogoutSuccessHandler {
+        @Override
+        public void onLogoutSuccess(HttpServletRequest request, HttpServletResponse response, Authentication authentication) throws IOException, ServletException {
+            response.setStatus(HttpStatus.OK.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+            response.getWriter().write("{\"status\":\"success\",\"message\":\"Logged out successfully.\"}");
+            response.getWriter().flush();
+        }
+    }
+
+}
+
